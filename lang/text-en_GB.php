e1bd1cf4f23789626ece38212c4f8374f7b12c50
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 10:54:14 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Date and time',
+    '{description}' => 'Date and time format',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Configuring Date and time format']
+    ],
+
+    // Form: поля
+    'Timezone' => 'Timezone',
+    'First day of the week' => 'First day of the week',
+    'Date format' => 'Date format',
+    'Time format' => 'Time format',
+    'Arbitrarily' => 'Arbitrarily',
+    'Details on date and time formats' => 'Details on date and time formats',
+    'Monday' => 'Monday',
+    'Tuesday' => 'Tuesday',
+    'Wednesday' => 'Wednesday',
+    'Thursday' => 'Thursday',
+    'Friday' => 'Friday',
+    'Saturday' => 'Saturday',
+    'Sunday' => 'Sunday',
+    'reset settings' => 'reset settings',
+    // Form: сообщения
+    'Save settings' => 'Save settings',
+    'Reset settings' => 'Reset settings',
+    'settings saved {0} successfully' => 'settings saved "<b>{0}</b>" successfully',
+    'settings reseted {0} successfully' => 'settings reseted "<b>{0}</b>" successfully',
+    // Form: сообщения / ошибки
+    'Cannot change settings (wrong tab id specified)' => 'Cannot change settings (wrong tab id specified).',
+    'No custom date format specified' => 'No custom date format specified',
+    'No custom time format specified' => 'No custom time format specified'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Дата и Время',
+    '{description}' => 'Формат даты и времени',
+    '{permissions}' => [
+        'any' => ['Полный доступ', 'Настройка формата даты и времени']
+    ],
+
+    // Form: поля
+    'Timezone' => 'Часовой пояс',
+    'First day of the week' => 'Первый день недели',
+    'Date format' => 'Формат даты',
+    'Time format' => 'Формат времени',
+    'Arbitrarily' => 'Произвольно',
+    'Details on date and time formats' => 'Подробно о форматах даты и времени',
+    'Monday' => 'Понедельник',
+    'Tuesday' => 'Вторник',
+    'Wednesday' => 'Среда',
+    'Thursday' => 'Четверг',
+    'Friday' => 'Пятница',
+    'Saturday' => 'Суббота',
+    'Sunday' => 'Воскресенье',
+    'reset settings' => 'сбросить настройки',
+    // Form: сообщения
+    'Save settings' => 'Сохранение настроек',
+    'Reset settings' => 'Сброс настроек',
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"',
+    'settings reseted {0} successfully' => 'успешный сброс настроек "<b>{0}</b>"',
+    // Form: сообщения / ошибки
+    'Cannot change settings (wrong tab id specified)' => 'Невозможно выполнить изменение настроек (неправильно указан идентификатор вкладки).',
+    'No custom date format specified' => 'Не указан произвольный формат даты',
+    'No custom time format specified' => 'Не указан произвольный формат времени'
+];
